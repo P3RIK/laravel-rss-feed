08bb56ebc94efd010ea46c97696344d0684b0e7b
p3rik <p3rikmax@gmail.com>
2022-12-01 18:13:53 +0100

    added layout, feed and form components

---
+++ resources/views/components/form.blade.php
@@ -0,0 +1,16 @@
+<div>
+    <form action="/form" method="post">
+        <div>
+            <label for="link">Enter link to RSS feed:</label>
+            <input type="text" name="link">
+        </div>
+        <div>
+            <label for="categories">Choose feed category:</label>
+            <select name="categories" id="cat">
+                <option value="tech">Technology</option>
+                <option value="pol">Politics</option>
+                <option value="sci">Science</option>
+            </select>
+        </div>
+    </form>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
+});
+
+Route::post('/form', function () {
+    return view('layouts.app');
 });
