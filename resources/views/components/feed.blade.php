59ea6ebb76272ec4f7723ce747c536cd9dbe400e
p3rik <p3rikmax@gmail.com>
2022-12-05 01:16:47 +0100

    added reset button for categories

--- resources/views/components/feed.blade.php
+++ resources/views/components/feed.blade.php
@@ -1,10 +1,12 @@
-<div>
+<div class="p-2">
     <h3>Feed</h3>
     <div class="dropdown show">
         <a class="btn btn-secondary dropdown-toggle" href="" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Categories
         </a>
 
+        <a class="btn btn-secondary" href="{{ route('home') }}">Reset</a>
+        
         <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
             @foreach($categories as $category)
             <a class="dropdown-item" href="{{ route('home', ['category' => $category->name]) }}">{{ $category->name }}</a>
@@ -12,7 +14,7 @@
         </div>
     </div>
     
-    @foreach($feed->get_items(0, 25) as $entry)
+    @foreach($feed as $entry)
         <x-feed-entry :entry='$entry' />
     @endforeach
 </div>
