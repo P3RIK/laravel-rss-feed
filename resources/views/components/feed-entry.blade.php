12655467236e79c58470ed2dd0ce4ef2cd66bbe1
p3rik <p3rikmax@gmail.com>
2022-12-06 23:44:10 +0100

    remade unsubscribe button

--- resources/views/components/feed-entry.blade.php
+++ resources/views/components/feed-entry.blade.php
@@ -1,19 +1,22 @@
 @php
-    $feed_name = $entry->get_feed()->get_title();
+    $channel_id = $entry->channel->id
 @endphp
 
 <div class="card mt-2">
-    <h5 class="card-header">{{ $feed_name }}</h5>
+    <h5 class="card-header">{{ $entry->channel->name }}</h5>
   
     <div class="card-body">
-        <h5 class="card-title">{{ $entry->get_title() }}</h5>
-        <a href="{{ $entry->get_link() }}" class="btn btn-primary btn-sm my-1">Link</a>
-        <p class="card-text">{!! $entry->get_description() !!}</p>
-        <p class="card-text d-flex justify-content-end">{{ $entry->get_date() }}</p>
+        <h5 class="card-title">{{ $entry->title }}</h5>
+        <a href="{{ $entry->link }}" class="btn btn-primary btn-sm my-1">Link</a>
+        <p class="card-text">{!! $entry->content !!}</p>
+        <p class="card-text d-flex justify-content-end">{{ $entry->publication_date }}</p>
     
         <div class="d-flex justify-content-end">
-            <a href="{{ route('index', $feed_name) }}" class="btn btn-danger">Unsubscribe</a>
+            <form action="/feed/{{ $channel_id }}" method='POST'>
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">Unsubscribe</button>
+            </form>
         </div>
-
     </div>
 </div>
